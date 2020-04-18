73a8d1eb3fe40770f1d5a0231b114aa310bd81c7
Vlad <vladdosiik7540@gmail.com>
2020-04-18 17:30:16 +0300

    update

--- app/src/main/java/com/example/todo_app/SplashScreen.java
+++ app/src/main/java/com/example/todo_app/SplashScreen.java
@@ -18,7 +18,7 @@ public class SplashScreen extends AppCompatActivity {
             public void run()
             {
                 try {
-                    Thread.sleep(2000);
+                   
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
