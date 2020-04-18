1e63877c54f54c68ec6c239e6587a351e5579f0a
Vlad <vladdosiik7540@gmail.com>
2020-04-18 17:23:13 +0300

    splash

--- app/src/main/java/com/example/todo_app/SplashScreen.java
+++ app/src/main/java/com/example/todo_app/SplashScreen.java
@@ -1,4 +1,38 @@
 package com.example.todo_app;
 
-public class SplashScreen {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import static java.lang.Thread.*;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Thread timerThread=new Thread()
+        {
+            public void run()
+            {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Intent intent=new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        };
+        timerThread.start();
+    }
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        finish();
+    }
 }
